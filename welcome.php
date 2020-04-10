d819227c163f31d04d628f47e7da52ca3b672f1e
vikartansood <38180337+vikartansood@users.noreply.github.com>
2020-04-10 12:04:00 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<?php
+// Initialize the session
+session_start();
+ 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hello! New User</b> </h1>
+        <h1>Welcome to our site.</h1>
+    </div>
+    
+</body>
+</html>
